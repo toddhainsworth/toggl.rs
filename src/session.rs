5f60b113a5dc4544b70ca6306cba3802aeac40c4
Todd Hainsworth <hainsworth.todd@gmail.com>
2019-02-16 14:42:07 +1030

    session stub

---
+++ src/session.rs
@@ -0,0 +1,29 @@
+pub struct Session {
+    api_key: String,
+}
+
+impl Session {
+    pub fn new(api_key: &'static str) -> Self {
+        Session {
+            api_key: api_key.to_string(),
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        !self.api_key.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Session;
+
+    #[test]
+    pub fn session_is_valid() {
+        let mut session = Session::new("abc123");
+        assert!(session.is_valid());
+
+        session.api_key = String::new();
+        assert!(!session.is_valid());
+    }
+}
