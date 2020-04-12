f02b2aea02495ccf6ee2010160a4b5f5a3b9aaca
Todd Hainsworth <hainsworth.todd@gmail.com>
2020-04-12 20:59:17 +0930

    update examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,11 +1,14 @@
 extern crate toggl;
 
-use toggl::entities::User;
+use toggl::entities::{Client, User};
 use toggl::session::Session;
 
 fn main() {
-    let session = Session::new("token-goes-here");
+    let session = Session::new("token goes here");
     let me = User::me(&session);
 
     println!("{:?}", me);
+
+    let client = Client::get(&session, "id goes here");
+    println!("{:?}", client);
 }
