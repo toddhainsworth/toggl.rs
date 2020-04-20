456b9cb3c9b1a4d70e87e5364322e9f81c34619c
Todd Hainsworth <todd.hainsworth@aligent.com.au>
2020-04-20 18:54:37 +0930

    group deletes

--- src/entities.rs
+++ src/entities.rs
@@ -559,12 +559,35 @@ impl Group {
         unimplemented!();
     }
 
-    pub fn delete() {
-        unimplemented!();
+    pub fn delete(self, session: &Session) -> Result<bool, Error> {
+        let id = self
+            .id
+            .as_ref()
+            .ok_or_else(|| TogglError::from("Cannot delete group with no ID"))?;
+        let url = format!("groups/{}", id);
+        http::delete(session, url)
+            .map(|r| r.status().is_success())
+            .map_err(Error::from)
     }
 
-    pub fn delete_by_ids() {
-        unimplemented!();
+    pub fn delete_by_ids(
+        self,
+        session: &Session,
+        ids: Vec<String>,
+    ) -> HashMap<String, Result<bool, Error>> {
+        // TODO: not totally sure what we should return here...was thinking just a
+        // Result<bool, Error> but that seems like it's too vague
+        ids.into_iter()
+            .map(|id| (id.clone(), Group::from_id(id).delete(session)))
+            .collect()
+    }
+
+    // Util function to create a default client from the given id.
+    // Note; no request to Toggl
+    pub fn from_id(id: String) -> Self {
+        let mut client = Group::default();
+        client.id = Some(id);
+        client
     }
 }
 
